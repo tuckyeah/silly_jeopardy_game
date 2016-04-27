93da76e64341bc341cbef8eb20c986569e61af40
Tuck Yeah <h.rosebrock@gmail.com>
2016-04-26 21:14:38 -0400

    got question asker working

--- jeopardy/game.py
+++ jeopardy/game.py
@@ -3,34 +3,30 @@ from random import randint
 
 class Question(object):
 
-	def __init__(self):
-		self.cat = get_category()
-		self.q = ""
-		self.ans = ""
+	def __init__(self, cat, qa):
+		self.cat = cat
+		self.q = qa[0] 
+		self.ans = qa[1]
 
+	def ask_question(self):
+		print "Okay PLAYER, the category is: \n"
+		print "%s\n" % self.cat
+		print "-" * 10
+		print "QUESTION:"
+		print "-" * 10
+		print self.q
+		print "-" * 10
 
 def get_question(cat): # gets random question() from dictionary.py
 	return dictionary.random_question(cat)
 
 def get_category(): # gets random_category() from dictionary.py
 	return dictionary.random_category()
-
-def ask_question():
-	qa_pair = 
 	
-
-def return_category(cat):
-	return cat
-
-
 
 cat = get_category()
 qa_pair = get_question(cat)
 
 question = Question(cat, qa_pair)
 
-print "Category: " 
-print question.cat
-print "Question: " 
-print question.q
-print "Answer: " + question.a 
+question.ask_question()
